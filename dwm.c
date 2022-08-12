9bb284e8ded0790eb3109b4cdcd532915fc56411
BYT0723 <1151713064@qq.com>
2022-08-13 01:23:57 +0800

    fix status update

--- dwm.c
+++ dwm.c
@@ -2446,11 +2446,10 @@ void updatesizehints(Client *c) {
 }
 
 void updatestatus(void) {
-    if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
+    if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
         strcpy(stext, "dwm-" VERSION);
-        drawbar(selmon);
-        updatesystray();
-    }
+    drawbar(selmon);
+    updatesystray();
 }
 
 
