ffde69d0552bd98e451c84163cae14e83460dddc
BYT0723 <twang9739@gmail.com>
2023-02-28 22:45:55 +0800

    pref(font): change font theme

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ static const unsigned int borderpx   = 3;   /* border pixel of windows */
 static const unsigned int snap       = 32;  /* snap pixel */
 static const          int showbar    = 1;   /* 0 means no bar */
 static const          int topbar     = 1;   /* 0 means bottom bar */
-static const          int barfontpad = 3;
+static const          int barfontpad = 4;
 static const          int vertpad    = 10;  /* vertical padding of bar */
-static const          int sidepad    = 5;  /* horizontal padding of bar */
+static const          int sidepad    = 10;  /* horizontal padding of bar */
 static const          char host[]    = " ";
 
 static const unsigned int gappih    = 19;  /* horiz inner gap between windows */
