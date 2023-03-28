59919563903b2deefcd7627fda9cc69b6e36ac39
BYT0723 <twang9739@gmail.com>
2023-03-29 07:36:16 +0800

    style(layout): change layout monocle position(x,y) and (width, height)

--- dwm.c
+++ dwm.c
@@ -1666,7 +1666,7 @@ void monocle(Monitor *m) {
   if (n > 0) /* override layout symbol */
     snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
   for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-    resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+    resize(c, m->wx + gappov, m->wy + gappoh, m->ww - 2 * c->bw - 2 * gappov, m->wh - 2 * c->bw - 2 * gappoh, 0);
 }
 
 void motionnotify(XEvent *e) {
