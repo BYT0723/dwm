19609748ce8ef7f603bd2df50a7f7b5701f525d7
BYT0723 <twang9739@gmail.com>
2023-03-31 23:10:36 +0800

    delete(script): remove alarm and monitor

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,10 +2,6 @@
 
 # statusBar
 /bin/bash ~/.dwm/dwm-status.sh &
-# clock notification
-/bin/bash ~/.dwm/alarm.sh &
-# file change monitor
-/bin/bash ~/.dwm/monitor.sh &
 
 # wallpaper
 /bin/bash ~/.dwm/background.sh &
@@ -22,10 +18,3 @@ nm-applet &
 # udiskie -tn &
 
 fcitx5 -d
-
-# proxy (privoxy with trojan [socks5])
-#
-# 1. cp ./configs/trojan.json /etc/trojan/config.json
-# 2. systemctl start trojan
-# 3. systemctl enable trojan
-# trojan -c ~/.dwm/configs/trojan-cli.json &
