55da7472de4543760c8d6e0e4a7a9988eba26494
BYT0723 <twang9739@gmail.com>
2023-04-05 04:50:49 +0800

    fix(script): volume value error

--- scripts/volume-toggle.sh
+++ scripts/volume-toggle.sh
@@ -6,7 +6,7 @@ status=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)\].*/\1/')
 
 amixer sset Master toggle
 if [ "$status" == "off" ]; then
-    notify-send -a "changeVolume" -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "${volume}"
+    notify-send -a "changeVolume" -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:$msgTag "${volume}"
 else
     notify-send -a "changeVolume" -i audio-volume-muted-symbolic -h string:x-dunst-stack-tag:$msgTag "muted"
 fi
--- scripts/volume-up.sh
+++ scripts/volume-up.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 msgTag="3"
-volume=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')
 
 /usr/bin/amixer -qM set Master 2%+ umute
 
-notify-send -a "changeVolume" -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "${volume}"
+volume=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')
+
+notify-send -a "changeVolume" -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:$msgTag "${volume}"
