db334ded4e04b2f849f6e40ab0cc10b8619827b9
BYT0723 <twang9739@gmail.com>
2023-04-19 00:08:09 +0800

    pref(script): add judgement to prevent break in autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -7,17 +7,34 @@
 /bin/bash ~/.dwm/background.sh &
 
 # picom (window composer)
-picom --config ~/.dwm/configs/picom.conf -b --experimental-backends
+if [ -z "$(pgrep picom)" ]; then
+    picom --config ~/.dwm/configs/picom.conf -b --experimental-backends
+fi
 
 # polkit (require lxsession or lxsession-gtk3)
-lxpolkit &
+if [ -z "$(pgrep lxpolkit)" ]; then
+    lxpolkit &
+fi
 
 # autolock (screen locker)
-xautolock -time 30 -locker betterlockscreen -detectsleep &
+if [ -z "$(pgrep xautolock)" ]; then
+    xautolock -time 30 -locker betterlockscreen -detectsleep &
+fi
 
-nm-applet &
+if [ -z "$(pgrep nm-applet)" ]; then
+    nm-applet &
+fi
+
 # mate-power-manager &
 # volumeicon &
-# udiskie -tn &
 
-fcitx5 -d
+if [ -z "$(pgrep fcitx5)" ]; then
+    fcitx5 -d
+fi
+
+sleep 3
+
+# 设置风扇转速为0
+if [ -x nbfc ]; then
+    nbfc set -s 0
+fi
