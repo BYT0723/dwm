fe62f8c07f6a66f554e4e9a77c7438c71e672665
BYT0723 <twang9739@gmail.com>
2023-04-25 15:16:45 +0800

    pref: mpd module notification

--- scripts/rofi/bin/mpd.sh
+++ scripts/rofi/bin/mpd.sh
@@ -102,22 +102,29 @@ run_rofi() {
     fi
 }
 
+msgTag="volume"
+
 # Execute Command
 run_cmd() {
     if [[ "$1" == '--on' ]]; then
         mpd
     elif [[ "$1" == '--opt1' ]]; then
-        mpc -q toggle && notify-send -u low "$prompt $(mpc status | awk 'NR==2 {print $1}')"
+        mpc -q toggle
+        notify-send -u low -i emblem-music-symbolic -h string:x-dunst-stack-tag:$msgTag "$(mpc current) $(mpc status | awk 'NR==2 {print $1}')"
     elif [[ "$1" == '--opt2' ]]; then
         mpc -q stop
     elif [[ "$1" == '--opt3' ]]; then
-        mpc -q prev && notify-send -u low " $(mpc -f "%title% - %artist%" current)"
+        mpc -q prev
+        notify-send -u low -i emblem-music-symbolic -h string:x-dunst-stack-tag:$msgTag "$(mpc -f "%title% - %artist%" current)"
     elif [[ "$1" == '--opt4' ]]; then
-        mpc -q next && notify-send -u low " $(mpc -f "%title% - %artist%" current)"
+        mpc -q next
+        notify-send -u low -i emblem-music-symbolic -h string:x-dunst-stack-tag:$msgTag "$(mpc -f "%title% - %artist%" current)"
     elif [[ "$1" == '--opt5' ]]; then
-        mpc volume -20 && notify-send -u low " $(mpc volume)"
+        mpc volume -20
+        notify-send -u low -i emblem-music-symbolic -h string:x-dunst-stack-tag:$msgTag "$(mpc volume)"
     elif [[ "$1" == '--opt6' ]]; then
-        mpc volume +20 && notify-send -u low " $(mpc volume)"
+        mpc volume +20
+        notify-send -u low -i emblem-music-symbolic -h string:x-dunst-stack-tag:$msgTag "$(mpc volume)"
     elif [[ "$1" == '--opt7' ]]; then
         mpc -q repeat
     elif [[ "$1" == '--opt8' ]]; then
