881489fdede27ae07bc048ca597110a04663eda0
BYT0723 <twang9739@gmail.com>
2023-05-16 00:19:49 +0800

    pref(install): pref install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
-DIR=$(pwd)
+DIR=$(dirname $0)
 
 # 编译dwm
 make clean install
 
-# 创建脚本软链接
-if [[ -d ~/.dwm ]]; then
-    mv ~/.dwm ~/.dwm-old
-fi
-ln -s ${DIR}/scripts/ ~/.dwm
+# 获取shell脚本
+git clone https://github.com/BYT0723/scripts.git ~/.dwm
 
 # 安装st
 if ! [[ -n $(command -v st) ]]; then
@@ -35,28 +32,29 @@ fi
 
 # clone configuration
 git clone https://github.com/BYT0723/dotfile.git
-mv -r ./dotfile/.config/* ~/.config/
+cd dotfile
+make install
+cd ..
 
 # 安装rofi
 if ! [[ -n $(command -v rofi) ]]; then
     pacman -S rofi
 fi
 
-# 安装picom-animation
+# 安装picom-animation-git
 # 如果非animations版本，将无法启用动画效果
-yay -S picom-animations-git
+pacman -S picom-git
 
-pacman -S mate-power-manager network-manager-applet volumeicon udiskie fcitx5-im privoxy trojan
+pacman -S network-manager-applet udiskie fcitx5-im privoxy trojan
 
 # theme
-# https://github.com/vinceliuice/WhiteSur-gtk-theme
+# git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
 
 # proxy
 cp ./scripts/configs/trojan.json /etc/trojan/config.json
-systemctl start trojan
-systemctl enable trojan
-
 cp ./scripts/configs/pac.action /etc/privoxy/pac.action
 echo "actionsfile pac.action" >>/etc/privoxy/config
-systemctl start privoxy
-systemctl enable privoxy
+# systemctl start trojan
+# systemctl enable trojan
+# systemctl start privoxy
+# systemctl enable privoxy
