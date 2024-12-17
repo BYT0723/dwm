8ce17755ad8ff8130aaf14defc62fae56224bbb0
BYT0723 <twang9739@gmail.com>
2024-12-17 21:59:57 +0800

    chore: add colorscheme

---
+++ colorschemes/tokyonight_day.h
@@ -0,0 +1,11 @@
+static const char col_bg[] = "#e1e2e7"; // background
+static const char col_fg[] = "#3760bf"; // foreground
+
+static const char col_black[] = "#b4b5b9";   /*  0: black    */
+static const char col_red[] = "#f52a65";     /*  1: red      */
+static const char col_green[] = "#587539";   /*  2: green    */
+static const char col_yellow[] = "#8c6c3e";  /*  3: yellow   */
+static const char col_blue[] = "#2e7de9";    /*  4: blue     */
+static const char col_magenta[] = "#9854f1"; /*  5: magenta  */
+static const char col_cyan[] = "#007197";    /*  6: cyan     */
+static const char col_white[] = "#6172b0";   /*  7: white    */
---
+++ colorschemes/tokyonight_storm.h
@@ -0,0 +1,11 @@
+static const char col_bg[] = "#24283b"; // background
+static const char col_fg[] = "#c0caf5"; // foreground
+
+static const char col_black[] = "#1d202f";   /*  0: black    */
+static const char col_red[] = "#f7768e";     /*  1: red      */
+static const char col_green[] = "#9ece6a";   /*  2: green    */
+static const char col_yellow[] = "#e0af68";  /*  3: yellow   */
+static const char col_blue[] = "#7aa2f7";    /*  4: blue     */
+static const char col_magenta[] = "#bb9af7"; /*  5: magenta  */
+static const char col_cyan[] = "#7dcfff";    /*  6: cyan     */
+static const char col_white[] = "#a9b1d6";   /*  7: white    */
